d74b1a9f94e0d42adbe871449d7ec105784b7713
tkrsh <utkarsh.srivastava123@outlook.com>
2021-07-05 21:24:42 +0530

    timer updated

--- pomodoro.py
+++ pomodoro.py
@@ -14,12 +14,10 @@ def display_timer(duration):
     duration = duration *60
     for remaining in range(duration, 0, -1):
         sys.stdout.write("\r")
-        sys.stdout.write("\t{:2d} seconds remaining.".format(remaining)) 
+        sys.stdout.write("{:2d} seconds remaining.".format(remaining)) 
         sys.stdout.flush()
         time.sleep(1)
-
     sys.stdout.write("\rComplete!            \n")
-
 
 def notify(msg,x):
     
