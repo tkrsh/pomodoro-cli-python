fdeafbc0ebf28358cd5fe1ff9898b2a2379d799f
tkrsh <utkarsh.srivastava123@outlook.com>
2021-07-05 21:20:37 +0530

    added visual timer

--- pomodoro.py
+++ pomodoro.py
@@ -4,14 +4,25 @@ import sys
 from tqdm import tqdm
  
  
-time_cycle = int(sys.argv[1]) *60 
-time_short_break = int(sys.argv[2]) *60
-time_long_break = int(sys.argv[3]) *60 
+time_cycle = int(sys.argv[1])   
+time_short_break = int(sys.argv[2])  
+time_long_break = int(sys.argv[3])  
 cycles= int(sys.argv[4])
 total_cycles = int(sys.argv[5]) 
 
+def display_timer(duration):
+    duration = duration *60
+    for remaining in range(duration, 0, -1):
+        sys.stdout.write("\r")
+        sys.stdout.write("\t{:2d} seconds remaining.".format(remaining)) 
+        sys.stdout.flush()
+        time.sleep(1)
+
+    sys.stdout.write("\rComplete!            \n")
+
 
 def notify(msg,x):
+    
     if msg ==0:
         os.system("notify-send CycleStarted")
     if msg ==1:
@@ -36,17 +47,20 @@ def pomodoro():
 
         for x in range(cycles):
             sys.stdout.write("\n\nPomodoro Cycle {} has started !\n".format(x+1))
-            time.sleep(time_cycle)
+            # time.sleep(time_cycle)
+            display_timer(time_cycle)
             notify(0,(x+1))
             sys.stdout.write("\n \tShort Break of {} (Minutes) Duration has started\n".format(time_short_break))            
-            time.sleep(time_short_break)
+            # time.sleep(time_short_break)
+            display_timer(time_short_break)
             notify(1,(x+1))
         
     
 
         sys.stdout.write("\nLong Break of {} (Minutes) Duration has started\n".format(time_long_break)) 
         notify(2,(i+1))
-        time.sleep(time_long_break)
+        # time.sleep(time_long_break)
+        display_timer(time_long_break)
         sys.stdout.write("\nComplete Cycle ({}/{}) of Pomodoro is done\n\n".format(i+1,total_cycles))
 
         if i+1 == total_cycles:
